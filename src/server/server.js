afc66a3654e1187775d8bd97f41b414dd13f5535
mike connor <mike@rocketnumber9.org>
2020-03-13 15:33:02 -0500

    cors

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,20 @@ const data = require('./data.json');
 const server = express();
 const port = 4000;
 
+
+const whitelist = ['http://zeit.co', 'http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 // GET question endpoint
-server.get("/api/questions", cors(), (req, res) => {
+server.get("/api/questions", cors(corsOptions),(req, res) => {
     res.json(data);
 });
 
