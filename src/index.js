f3e46d1f3d5895348cf485c8b6f5f7237f0c1f87
mike connor <mike@rocketnumber9.org>
2020-03-13 21:29:54 -0500

    json on gist

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { App } from './client/App';
 
 ReactDOM.render(<App 
-        api='http://localhost:4000/api/questions'
+        api='https://gist.githubusercontent.com/mconnor/57fdef47f97e39741412215fa4d669c9/raw/c3453dcdcc571030cdaca1d6a475ba106f1933e8/politicalTrivia.json'
         geoURL = 'https://api.ipdata.co/2601:8:be00:cf20:ca60:ff:fe09:35b5?api-key='
         eMailContact='mike@cloudswing.info'
         devName= 'Mike Connor'
