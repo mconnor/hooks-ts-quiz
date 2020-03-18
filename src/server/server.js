2c1e9101a932c849cb45380903830ca3cb0f251b
mike connor <mike@rocketnumber9.org>
2020-03-18 11:18:23 -0500

    now config

--- src/server/server.js
+++ src/server/server.js
@@ -8,19 +8,9 @@ const server = express();
 const port = 4000;
 
 
-const whitelist = ['https://hooks-ts-quiz-ink1kt0jq.now.sh','https://hooks-ts-quiz.mconnor.now.sh/','http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 
 // GET question endpoint
-server.get("/api/questions", cors(corsOptions),(req, res) => {
+server.get("/api/questions", cors(),(req, res) => {
     res.json(data);
 });
 
