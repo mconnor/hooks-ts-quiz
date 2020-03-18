ca12ab7981bdd674eaa969655dfb7114e763e76c
mike connor <mike@rocketnumber9.org>
2020-03-18 18:16:35 -0500

    move files

---
+++ api/quiz.ts
@@ -0,0 +1,6 @@
+import * as data from './data.json';
+import { NowRequest, NowResponse } from '@now/node';
+
+export default (_req: NowRequest, res: NowResponse) => {
+    res.json(data as any);
+};
--- src/api/quizData.js
+++
@@ -1,7 +0,0 @@
-const data = require('./data.json');
-
-module.exports = (req, res) => {
-    const jsonData = res.json(data);
-    res.status(200).send(jsonData);
-  };
-  
