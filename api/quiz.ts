e9015a232c1feb50e46578eef52a468bafd96490
mike connor <mike@rocketnumber9.org>
2020-03-19 10:38:10 -0500

    now dev

--- api/quiz.ts
+++ api/quiz.ts
@@ -1,6 +1,8 @@
 import * as data from './data.json';
 import { NowRequest, NowResponse } from '@now/node';
 
+
+  
 export default (_req: NowRequest, res: NowResponse) => {
-    res.json(data as any);
+    res.status(200).json(data as any);
 };
